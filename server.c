f0088dd05edb2657b5e5c83866fc4045ccd3b184
Sarah Pham <spham@myocv.com>
2020-11-05 12:41:08 -0600

    Finished up server.c, however there is server binding errors

--- server.c
+++ server.c
@@ -71,6 +71,46 @@ int connectToClient(){
     return 0;
 }
 
+/**
+ * parseStrToToken - method that will parse any given string to a sequence of tokens, loops through the string that is split by any given deliminator
+ * Parameters:
+ * pointer - value of end of token, tells us when to break out of the loop
+ * buffer - our string buffer 
+*/
+char* parseStrToToken(int pointer, char *buffer, char* deliminator){
+    int i = 0;
+    char *temp = NULL;
+
+    // strtok_r is used to break up the string by delimiters
+    char *token;
+    token = strtok_r(buffer, deliminator , &temp); 
+
+    while(token != NULL){
+        if(i == pointer) break;
+        i++;
+
+        token = strtok_r(NULL, deliminator, &temp);
+    }
+
+    return token;
+}
+
+/**
+ * calculateChecksum will calculate the sum of the data inside the packet and will return true if the sum is equal to the checksum
+ * Parameters:
+ * bufferHolder - the packet including all the data
+ * checksum - sum of all bytes within the packet
+ */
+int calculateChecksum(char* bufferHolder, int checksum){
+    int i = 0, sum = 0;
+
+    while(bufferHolder[i] != '\0'){
+        sum += bufferHolder[i];
+        i++;
+    }
+
+    return checksum == sum; 
+}
 
 /**
 *   writeToFile creates a file and transmits the message to another socket
@@ -84,7 +124,8 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
 
     
     char buffer[MAX_SIZE];
-    int character = 1;
+    char bufferHolder[MAX_SIZE - 12];
+    int character = 1, checksum, numOfPackets;
 
     
 
@@ -93,8 +134,7 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
     // recvfrom receives a message from a socket, retrieves source address of received data
     character = recvfrom(serverSocket, buffer, MAX_SIZE, MSG_WAITALL, (struct sockaddr *)&temp_address, &addressSize);
     printf("%s\n", buffer);
-    char* newFile;
-    newFile = parseStrToToken(1, buffer);
+    char* newFile = parseStrToToken(1, buffer, " ");
     file = fopen(newFile, "w");
 
     // bzero erases data in the MAX_SIZE bytes of the memory starting at buffer location by writing zeros
@@ -108,33 +148,31 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
         if(buffer[0] == '\0') break;
     }
 
+    // sendto will send a message on a socket, from library socket.h
+    sendto(serverSocket, (const char *)buffer, MAX_SIZE, 2048, (const struct sockaddr *)&socketStorage, addressSize);
+
+
+    char holder_1[MAX_SIZE];
+    char holder_2[MAX_SIZE];
+    
+    sprintf(holder_1, "%s", buffer);
+    sprintf(holder_2, "%s", buffer);
+
+    sprintf(bufferHolder, "%s", parseStrToToken(2, buffer, "|"));
+    fprintf(file, "%s", bufferHolder);
+
+    checksum = atoi(parseStrToToken(1, holder_1, "|"));
+    numOfPackets = atoi(parseStrToToken(0, holder_2, "|"));
+
+    if(!calculateChecksum(bufferHolder, checksum)) printf("This packet has an error!");
+
+    bzero(buffer, MAX_SIZE);
+
     return;
 }
 
 
-/**
- * parseStrToToken - method that will parse any given string to a sequence of tokens, loops through the string that is split by whitespaces
- * Parameters:
- * pointer - value of end of token, tells us when to break out of the loop
- * buffer - our string buffer 
-*/
-char *parseStrToToken(int pointer, char *buffer){
-    int i = 0;
-    char *temp;
 
-    // strtok_r is used to break up the string by delimiters
-    char *token;
-    token = strtok_r(buffer, " ", &temp); 
-
-    while(token != NULL){
-        if(i == pointer) break;
-        else i++;
-
-        token = strtok_r(NULL, " ", &temp);
-    }
-
-    return token;
-}
 
 int main(void){
     connectToClient();
