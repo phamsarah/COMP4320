c9c8f19fbdebd8a7e481d01b666a2d7492df1cfd
Jonathan <jwo.2017@gmail.com>
2020-11-05 20:21:58 -0600

    Server seems to work, needs to be tested w client

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 #include <strings.h>
 #include <sys/types.h>
 
+
 #define MAX_SIZE 128
 
 /**
@@ -14,10 +15,10 @@
  * 
  **/
 
-int serverSocket;
+int sockfd, newSocket;
 
 // sockaddr_in is a predefined struct in the in.h library, contains the information about the address coming in
-struct sockaddr_in serverAddress;
+struct sockaddr_in serverAddress, peerAddr;
 
 // sockaddr_storage is from the socket.h library, defined to declare storage for automatic variables that is large and aligned enough for the socket address data structure of ANY family
 struct sockaddr_storage socketStorage;
@@ -27,29 +28,35 @@ socklen_t socketAddressSize;
 
 int connectToClient(){
     /** 
-     * Socket method returns a nonnegative number integer if successful. Creates an unbound socket in a communications domain, and returns a file descriptor that can be used in later function calls that operate on sockets
+     * Socket method returns a nonnegative number integer if successful. Creates an unbound socket in a communications domain, 
+     * and returns a file descriptor that can be used in later function calls that operate on sockets
      * 
      *   PF_INET = protocol family that refers to anything in the protocol, usually sockets/ports
      *   SOCK_DGRAM = is a datagram based protocol used by UDP; send one datagram and receive one reply then connection terminates
      *   0 = protocol of 0 causes socket() to use an unspecified default protocol appropriate for the requested socket type
     **/ 
-    if((serverSocket = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+   //getaddrinfo(NULL, "3490", &serverAddress);
+
+   char *ip = "127.0.0.1";
+   int portNo = 8080;
+    if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
         // errx produces an error message
         errx(1, "Socket call error");
         printf("Error calling socket");
         exit(EXIT_FAILURE);
     }
 
-    printf("Success: Server Socket %i Call\n", serverSocket);
+    printf("Success: Server Socket %i Call\n", sockfd);
 
 
-    serverAddress.sin_family = AF_INET; // Binding the socket with an address so that the remote process can refer to it
-    serverAddress.sin_port = htons(9002); // Binding a port number to the socket
+    serverAddress.sin_family = AF_INET; // Setting family to INET
+    serverAddress.sin_port = htons(portNo); // Binding a port number to the socket
     serverAddress.sin_addr.s_addr = INADDR_ANY; // sin_addr is the socket ip address, we set the current host IP address: INADDR_ANY. 
 
     //bzero((char *) &serverAddress, sizeof (serverAddress));
     /**
-     * bind method in socket.h library assigns a local socket addr to a socket identified by descriptor socket, which has no local socket addr assigned
+     * bind method in socket.h library assigns a local socket addr to 
+     * a socket identified by descriptor socket, which has no local socket addr assigned
      * - basically binds a name to a socket
      * - returns 0 if sucessful and -1 otherwise
      * 
@@ -58,19 +65,59 @@ int connectToClient(){
      * address - points to a sockaddr structure containing address to be bound
      * address_len - specifies length of sockaddr structure
      **/
-    if(bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
-        errx(1, "Socket address binding error");
+    if(bind(sockfd, (struct sockaddr *) &serverAddress, sizeof(serverAddress)) < 0){
+        errx(1, "Socket address binding error\n");
         printf("socket binding error\n");
         exit(EXIT_FAILURE);
     }
 
-    printf("Success: Socket Address Binding");
+    printf("Success: Socket Address Binding\n");
 
     socketAddressSize = sizeof(socketStorage); // We initialize a new socket that is the size of the socket storage
 
     return 0;
 }
 
+/**
+ * parseStrToToken - method that will parse any given string to a sequence of tokens, loops through the string that is split by any given deliminator
+ * Parameters:
+ * pointer - value of end of token, tells us when to break out of the loop
+ * buffer - our string buffer 
+*/
+char* parseStrToToken(int pointer, char *buffer, char* deliminator){
+    int i = 0;
+    char *temp = NULL;
+
+    // strtok_r is used to break up the string by delimiters
+    char *token;
+    token = strtok_r(buffer, deliminator , &temp); 
+
+    while(token != NULL){
+        if(i == pointer) break;
+        i++;
+
+        token = strtok_r(NULL, deliminator, &temp);
+    }
+
+    return token;
+}
+
+/**
+ * calculateChecksum will calculate the sum of the data inside the packet and will return true if the sum is equal to the checksum
+ * Parameters:
+ * bufferHolder - the packet including all the data
+ * checksum - sum of all bytes within the packet
+ */
+int calculateChecksum(char* bufferHolder, int checksum){
+    int i = 0, sum = 0;
+
+    while(bufferHolder[i] != '\0'){
+        sum += bufferHolder[i];
+        i++;
+    }
+
+    return checksum == sum; 
+}
 
 /**
 *   writeToFile creates a file and transmits the message to another socket
@@ -84,7 +131,8 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
 
     
     char buffer[MAX_SIZE];
-    int character = 1;
+    char bufferHolder[MAX_SIZE - 12];
+    int character = 1, checksum, numOfPackets;
 
     
 
@@ -93,8 +141,7 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
     // recvfrom receives a message from a socket, retrieves source address of received data
     character = recvfrom(serverSocket, buffer, MAX_SIZE, MSG_WAITALL, (struct sockaddr *)&temp_address, &addressSize);
     printf("%s\n", buffer);
-    char* newFile;
-    newFile = parseStrToToken(1, buffer);
+    char* newFile = parseStrToToken(1, buffer, " ");
     file = fopen(newFile, "w");
 
     // bzero erases data in the MAX_SIZE bytes of the memory starting at buffer location by writing zeros
@@ -108,38 +155,36 @@ void writeToFile(int serverSocket, struct sockaddr_storage socketStorage){
         if(buffer[0] == '\0') break;
     }
 
+    // sendto will send a message on a socket, from library socket.h
+    sendto(serverSocket, (const char *)buffer, MAX_SIZE, 2048, (const struct sockaddr *)&socketStorage, addressSize);
+
+
+    char holder_1[MAX_SIZE];
+    char holder_2[MAX_SIZE];
+    
+    sprintf(holder_1, "%s", buffer);
+    sprintf(holder_2, "%s", buffer);
+
+    sprintf(bufferHolder, "%s", parseStrToToken(2, buffer, "|"));
+    fprintf(file, "%s", bufferHolder);
+
+    checksum = atoi(parseStrToToken(1, holder_1, "|"));
+    numOfPackets = atoi(parseStrToToken(0, holder_2, "|"));
+
+    if(!calculateChecksum(bufferHolder, checksum)) printf("This packet has an error!");
+
+    bzero(buffer, MAX_SIZE);
+
     return;
 }
 
 
-/**
- * parseStrToToken - method that will parse any given string to a sequence of tokens, loops through the string that is split by whitespaces
- * Parameters:
- * pointer - value of end of token, tells us when to break out of the loop
- * buffer - our string buffer 
-*/
-char *parseStrToToken(int pointer, char *buffer){
-    int i = 0;
-    char *temp;
 
-    // strtok_r is used to break up the string by delimiters
-    char *token;
-    token = strtok_r(buffer, " ", &temp); 
-
-    while(token != NULL){
-        if(i == pointer) break;
-        else i++;
-
-        token = strtok_r(NULL, " ", &temp);
-    }
-
-    return token;
-}
 
 int main(void){
     connectToClient();
-    printf("Server Socket: %i", serverSocket);
-    writeToFile(serverSocket, socketStorage);
+    printf("Server Socket: %i", sockfd);
+    writeToFile(sockfd, socketStorage);
 
     return 0;
 }
